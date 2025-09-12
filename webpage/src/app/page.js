5f4a591791a325422aac974b54228ad42376778d
benbrookhart <benbrookhart89@gmail.com>
2025-09-12 12:55:02 -0400

    Testing push to new branch

--- webpage/src/app/page.js
+++ webpage/src/app/page.js
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            BRANCH TEST BRANCH TEST {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.js
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save Omar.
+            BRANCH TEST BRANCH TEST!!!
           </li>
         </ol>
 
