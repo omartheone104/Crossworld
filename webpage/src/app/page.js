e982e8d45ee3d7dcc2a7b8629c1c827da3ef94bb
benbrookhart <benbrookhart89@gmail.com>
2025-09-16 18:37:53 -0400

    We are doing click events, Go backend dropping soon

--- webpage/src/app/page.js
+++ webpage/src/app/page.js
@@ -1,11 +1,36 @@
+"use client";
 import Image from "next/image";
 
+// export default function ClientSideFetch() {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetch('localhost:8080/creditscore')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setData(data);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) return <p>Loading...</p>;
+//   return JSON.stringify(data, null, 2);
+// }
 export default function Home() {
+  function handleClick() {
+    // alert("WOW");
+    const myElement = document.getElementById("text1");
+    if (myElement) {
+      myElement.innerText = "You chose!";
+    }
+    // var data = ClientSideFetch()
+    // alert(data)
+  }
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          //className="dark:invert"
           src="/omah.png"
           alt="OMAR"
           width={399}
@@ -13,84 +38,38 @@ export default function Home() {
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
+          <li id="text1" className="mb-2 tracking-[-.01em]">
             BRANCH TEST BRANCH TEST {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.js
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
+          <li id="text2" className="tracking-[-.01em]">
             BRANCH TEST BRANCH TEST!!!
           </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <button
+            onClick={handleClick} 
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://www.balatrodle.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             Destroy Omar
-          </a>
-          <a
+          </button>
+          <button
+            onClick={handleClick} 
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://www.balatrodle.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Save Omar
-          </a>
+          </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.balatrodle.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.balatrodle.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.balatrodle.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+ 
